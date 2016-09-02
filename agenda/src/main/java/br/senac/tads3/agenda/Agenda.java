ff74e41ad66c6ed59b4cb2d3c8ce24ee79a694df
Erik <Erik@Erik-PC.Home>
2016-09-01 23:28:01 -0300

    Atualização do método alterar

--- agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
@@ -43,13 +43,77 @@ public class Agenda extends ConexaoBD {
   }
   
   
-  public void alterar(){
-      // 1) Abrir conexao
+public void alterar(){
+      
+    System.out.print("Digite o ID do contato: ");
+    String id = entrada.nextLine();
+      
+    System.out.print("Digite o nome completo do contato: ");
+    String nome = entrada.nextLine();
+    
+    System.out.print("Digite a data de nascimento no formato dd/mm/aaaa: ");
+    String strDataNasc = entrada.nextLine();
+    
+    System.out.print("Digite o e-mail");
+    String email = entrada.nextLine();
+    
+    System.out.print("Digite o telefone no formato 99 99999-9999");
+    String telefone = entrada.nextLine();
+    
+    // 1) Abrir conexao
     PreparedStatement stmt = null;
-    Connection conn = null;  
-     
-  }
+    Connection conn = null;
+    
+    String sql = "UPDATE TB_CONTATO SET NM_CONTATO =?, DT_NASCIMENTO = ?, "
+   + "VL_TELEFONE =?, VL_EMAIL = ?, DT_CADASTRO =? WHERE ID_CONTATO =? ";
+    
+    try {
+      conn = obterConexao();
+      stmt = conn.prepareStatement(sql);
+      stmt.setString(1, nome);
+      
+      DateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
+      Date dataNasc = null;
+      try {
+		dataNasc = formatador.parse(strDataNasc);
+			  } catch (ParseException ex) {
+		System.out.println("Data de nascimento inválida.");
+		return;
+      }
+      stmt.setDate(2, new java.sql.Date(dataNasc.getTime()));
+      stmt.setString(3, telefone);
+      stmt.setString(4, email);
+      stmt.setDate(5, new java.sql.Date(System.currentTimeMillis()));
+      stmt.setString(6, id);
+      
+      // 2) Executar SQL
+      stmt.executeUpdate();
+      System.out.println("Contato alterado com sucesso");
+ 
+    } catch (SQLException e) {
+      System.out.println("Não foi possível executar.");
+    } catch (ClassNotFoundException e) {
+      System.out.println("Não foi possível executar.");
+    } finally {
+      if (stmt != null) {
+	try {
+	 stmt.close();
+	} catch (SQLException ex) {
+	 System.out.println("Erro ao fechar stmt.");
+	}
+		  }
+		  if (conn != null) {
+	try {
+	 conn.close();
+	} catch (SQLException ex) {
+	 System.out.println("Erro ao fechar conn.");
+	}
+		  }
+	}
+      
   
+  }
+
   public void incluir() {
     
     System.out.print("Digite o nome completo do contato: ");
